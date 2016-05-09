54594429ef04f788b218a7b3cf9946abfbc0271b
Kevin Krier <kkrier@twilio.com>
2016-05-08 19:39:07 -0700

    added inbound only

--- pullInboundMessageLogs.js
+++ pullInboundMessageLogs.js
@@ -2,9 +2,9 @@
 ////           Call script with: node pullMessageLogs.js SID Auth StartDate(YYYY-MM-DD) EndDate(YYYY-MM-DD)               ////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-if(!(process.argv[2]&&process.argv[3]&&process.argv[4]&&process.argv[5]&&process.argv[6])) {
+if(!(process.argv[2]&&process.argv[3]&&process.argv[4]&&process.argv[5])) {
 
-	console.log("Please pass in the following variables: SID, Auth, Start Date, End Date, Error Code");
+	console.log("Please pass in the following variables: SID, Auth, Start Date, End Date");
 	process.exit(1);
 
 }
@@ -26,11 +26,8 @@ var auth = process.argv[3];
 var startDate = moment(process.argv[4], 'YYYY-MM-DD');
 var endDate = moment(process.argv[5], 'YYYY-MM-DD');
 
-//Error code to filter one
-var errorCode = process.argv[6];
-
 //Output
-var stream = fs.createWriteStream('Message log for ' + sid + ': '+ startDate.format('YYYY-MM-DD') + ' to ' + endDate.format('YYYY-MM-DD') + '.csv');
+var stream = fs.createWriteStream('Inbound message log for ' + sid + ': '+ startDate.format('YYYY-MM-DD') + ' to ' + endDate.format('YYYY-MM-DD') + '.csv');
 
 //Initial variables
 var baseUrl = 'https://api.twilio.com/2010-04-01/Accounts/' + sid + '/Messages.json?PageSize=1000&Page=0';
@@ -42,7 +39,7 @@ var auth = {
 
 var parameters = {}
 
-stream.write('From,To,Direction,Num Segments,Price,Date,Number Type,Error Code,Carrier,SID\n');
+stream.write('From,To,Direction,Num Segments,Price,Date,Status,Error Code,To Country,From Country,SID\n');
 
 //Start pulling logs for each day in the time period selected
 for (var m = startDate; m.isBefore(endDate); m.add(1, 'days')) {
@@ -63,18 +60,33 @@ function pullMessages(uri) {
 
 		async.forEachOf(JSON.parse(body).messages, function(item,key){
 
-			if(item.error_code==errorCode){
+			if(item.direction == 'inbound') {
+				//Assign countries or Short Code based on country code/number length
+				//Have to use try/catch blocks because the ported libphonenumber I'm using doesn't have callbacks
+				try {
+					var fromCountry = phoneUtil.getRegionCodeForNumber(phoneUtil.parse(item.from));
+				}
+				catch (error) {
+					if(item.from ? (item.from.length==5 || item.from.length==6) : false) {
+						var fromCountry = 'Short Code'
+					} else {
+						var fromCountry = 'Unk'
+					}
+				}
 				
-				request.get({auth: auth, uri: 'https://lookups.twilio.com/v1/PhoneNumbers/' + item.to + '?Type=carrier&CountryCode=US'}, function(error, httpResponse, body){
+				try {
+					var toCountry = phoneUtil.getRegionCodeForNumber(phoneUtil.parse(item.to));
+				}
+				catch (error) {
+					if(item.to ? (item.to.length==5 || item.to.length==6) : false) {
+						var toCountry = 'Short Code'
+					} else {
+						var toCountry = 'Unk'
+					}
+				}
 
-  					stream.write(item.from+','+item.to+','+item.direction+','+item.num_segments+','+item.price+','+item.date_created+','+JSON.parse(body).carrier.type+','+JSON.parse(body).carrier.name+','+item.error_code+','+item.sid+'\n');
-
-				});
-				
-
+				stream.write(item.from+','+item.to+','+item.direction+','+item.num_segments+','+item.price+','+item.date_created+','+item.status+','+item.error_code+','+fromCountry+','+toCountry+','+item.sid+'\n');
 			}
-			
-
 		});
 
 		console.log(JSON.parse(body).next_page_uri);
